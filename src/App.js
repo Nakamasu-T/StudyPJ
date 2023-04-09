fec893be8155151edac754651308c03655802730
Nakamasu-T <comicalparade@gmail.com>
2023-04-09 11:42:42 +0900

    TodoList_React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
 
-function App() {
-  return (
-    <div className="App">
-      <p>テスト</p>
-      <button>aaa
-      </button>
-      <form action="/name" method= "post">
-        <label>
-         Name:
-         <input type="text" name="name" /> 
-        </label>
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  );
+function App(){
+    return (
+            <div style={{ margin: "2em"}}>
+                    <Todo />
+            </div>
+    );
 }
 
 export default App;
